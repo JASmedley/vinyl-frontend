17f5d23ca35e46aa4bfd4593eaefff04edc7d2b9
JASmedley <jasmine.a.smedley@gmail.com>
2024-02-27 20:05:05 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import {useEffect, useState} from 'react';
+import './App.css';
+import Items from './Item'
+
+function App() {
+  const [data,setData] = useState([])
+  useEffect(()=> {
+    console.log('Mounted')
+
+    fetch("https://vinyl-capstoner.vercel.app/vinyl")
+    .then((response) => response.json())
+    .then((data)=> setData(data));
+  }, [])
+
+  useEffect(()=> {
+    console.log({data}), [data]
+  })
+  return (
+  <>
+ <Items data={data}/>  
+ </>
+  )
+}
+
+export default App
